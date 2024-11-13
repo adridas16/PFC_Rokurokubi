9e5ffcdc7b69b02a11351baa62cb8b3fbdeb7ae5
adridas16 <adridas666@gmail.com>
2024-11-13 21:23:17 +0100

    quest

---
+++ Assets/Scrips/Task/QuestGiver.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestGiver : MonoBehaviour
+{
+    [SerializeField] private Task task;
+    [SerializeField] private PlayerControler PlayerCon;
+}
