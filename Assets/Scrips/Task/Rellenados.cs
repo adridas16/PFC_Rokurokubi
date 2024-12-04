20f105e3b1fa4dd684af7b34e30d5898bb35f6fe
adridas16 <adridas666@gmail.com>
2024-12-04 19:59:01 +0100

    mision2Rellenar

---
+++ Assets/Scrips/Task/Rellenados.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rellenados : MonoBehaviour
+{
+
+     MeshRenderer m_RendererAzucar;
+     MeshRenderer m_RendererCafe;
+     MeshRenderer m_RendererHarina;
+    [SerializeField] private Material m_Cafe;
+    [SerializeField] private Material m_Azucar;
+    [SerializeField] private Material m_Harina;
+    
+    bool YaAzucar =true;
+    bool YaCafe =true;
+    bool YaHarina =true;
+
+    public bool yaHarina { get => YaHarina; set => YaHarina = value; }
+    public bool yaCafe { get => YaCafe; set => YaCafe = value; }
+    public bool yaAzucar { get => YaAzucar; set => YaAzucar = value; }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (CompareTag("RellenarAzucar"))
+        {
+            m_RendererAzucar = GetComponent<MeshRenderer>();
+            Debug.Log("1");
+        }
+        if (CompareTag("RellenarCafe"))
+        {
+            m_RendererCafe = GetComponent<MeshRenderer>();
+            Debug.Log("2");
+        }
+        if (CompareTag("RellenarHarina"))
+        {
+            m_RendererHarina = GetComponent<MeshRenderer>();
+            Debug.Log("3");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("RellenableHarina"))
+        {
+            m_RendererHarina.material = m_Harina;
+        }
+        if (collision.gameObject.CompareTag("RellenableAzucar"))
+        {
+            m_RendererAzucar.material = m_Azucar;
+        }
+        if (collision.gameObject.CompareTag("RellenableCafe"))
+        {
+            m_RendererCafe.material = m_Cafe;
+        }
+
+    }
+
+}
---
+++ Assets/Scrips/Task/RellenarElementos.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RellenarElementos : MonoBehaviour
+{
+    [SerializeField] private TMP_Text RellenarText;
+    private static int misionCumplida;
+    [SerializeField] Material m_Vacio; 
+    MeshRenderer m_RendererRellenarAzucar;
+    MeshRenderer m_RendererRellenarCafe;
+    MeshRenderer m_RendererRellenarHarina;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        if (CompareTag("RellenableCafe"))
+        {
+            m_RendererRellenarCafe= GetComponent<MeshRenderer>();
+        }
+        if (CompareTag("RellenableAzucar"))
+        {
+            m_RendererRellenarAzucar= GetComponent<MeshRenderer>();
+        }
+        if (CompareTag("RellenableHarina"))
+        {
+            m_RendererRellenarHarina= GetComponent<MeshRenderer>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rellenado();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (CompareTag("RellenableCafe"))
+        {
+            if (collision.gameObject.CompareTag("RellenarCafe"))
+            {
+                m_RendererRellenarCafe.material = m_Vacio;
+                if (!collision.gameObject.GetComponent<Rellenados>().yaCafe)
+                {
+                    
+                    misionCumplida++;
+                    collision.gameObject.GetComponent<Rellenados>().yaCafe = true;
+                    
+                }
+
+
+
+            }
+        }
+        if (CompareTag("RellenableAzucar"))
+        {
+            if (collision.gameObject.CompareTag("RellenarAzucar"))
+            {
+                m_RendererRellenarAzucar.material = m_Vacio;
+                if (!collision.gameObject.GetComponent<Rellenados>().yaAzucar)
+                {
+                   
+                    misionCumplida++;
+                    collision.gameObject.GetComponent<Rellenados>().yaAzucar = true;
+
+                }
+
+
+
+            }
+        }
+            
+        if (CompareTag("RellenableHarina"))
+        {
+            if (collision.gameObject.CompareTag("RellenarHarina"))
+            {
+                m_RendererRellenarHarina.material = m_Vacio;
+                if (!collision.gameObject.GetComponent<Rellenados>().yaHarina)
+                {
+                    
+                    misionCumplida++;
+                    collision.gameObject.GetComponent<Rellenados>().yaHarina = true;
+
+                }
+
+
+
+            }
+        }
+            
+    }
+    private void Rellenado()
+    {
+        if (misionCumplida == 3)
+        {
+            RellenarText.text = "2";
+        }
+    }
+}
